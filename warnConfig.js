d2d321a32c7f6d8003719e9dbbe27e100e052f0b
MrPluto <457808167@qq.com>
2020-01-03 15:24:59 +0800

    ddd

---
+++ warnConfig.js
@@ -0,0 +1,23 @@
+//发送者邮箱厂家
+let EmailService = "qq";
+//发送者邮箱账户SMTP授权码
+let EmailAuth = {
+  user: "457808167@qq.com",
+  pass: "bwyppuzvonejbjjj"
+};
+//发送者昵称与邮箱地址
+let EmailFrom = "<457808167@qq.com>";
+
+//接收者邮箱地
+let EmailTo = "2272705341@qq.com";
+
+let EmailSubject = "报警邮件";
+
+module.exports = {
+  EmailService,
+  EmailAuth,
+  EmailFrom,
+  EmailTo,
+  EmailSubject
+};
+
