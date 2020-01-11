885ea063aba1c0f90bc72deb74e53c0af9a5b55e
MrPluto <457808167@qq.com>
2020-01-11 11:47:51 +0800

    test dy

--- test.js
+++ test.js
@@ -6,6 +6,8 @@ const fs = require("fs"); //文件读写
 const path = require("path"); //路径配置
 const config = require("./warnConfig");
 const { EmailService, EmailAuth, EmailFrom, EmailTo, EmailSubject } = config;
+const DY = require("./dySign");
+const { generateSignature } = DY;
 
 // 发动邮件
 function sendMail(message, useragent) {
@@ -51,6 +53,36 @@ app.get("/report", (req, res) => {
   }
   sendMail(message, useragent);
   res.send("请求成功");
+});
+
+app.get("/dysign", (req, res) => {
+  let query = req.query;
+  var url = query.url;
+  var tac = query.tac;
+  console.log(tac);
+  console.log(url);
+
+  // if (!url || !tac) {
+  //   res.json({
+  //     code: -1,
+  //     message: "缺少url或者tac"
+  //   });
+  //   return;
+  // }
+  // tac =
+  //   'i+2gv0xzhi6rqs!i#fz6s"yZl!%s"l"u&kLs#l l#vr*charCodeAtx0[!cb^i$1em7b*0d#>>>s j￮l  s#';
+  let p = {
+    url,
+    bodyVal2str: false
+  };
+  let signature = generateSignature(p, tac);
+  res.json({
+    code: 200,
+    data: {
+      signature,
+      url
+    }
+  });
 });
 
 app.listen(3888, () => console.log("Example app listening on port 3888!"));
